25945f428477b3d042da339454579333c2eaa118
culnoty <culnoty@outlook.com>
2017-06-23 19:10:17 +0800

    demo

---
+++ index.js
@@ -0,0 +1,19 @@
+var fontCarrier = require('font-carrier');
+var fs = require('fs');
+//创建空白字体，使用svg生成字体
+var font = fontCarrier.create()
+var love = fs.readFileSync('./input/家庭.svg').toString()
+var mail = fs.readFileSync('./input/箭头-右.svg').toString()
+
+//使用汉字
+font.setGlyph('家',{
+  svg:love,
+  glyphName:'家'
+})
+
+//使用unicode
+font.setSvg('&#xe601;',mail)
+
+font.output({
+  path:'./output/iconfont'
+})
